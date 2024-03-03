4826f829f0ddabe39a6f6f3d52644b73cdaccbf3
Amirametwally <amira.metwally56@gmail.com>
2024-03-03 05:06:36 +0200

    Cash only

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,13 @@
 $(document).ready(() => {
-  const amenities = {}; // Dictionary to store the checked amenities
+  const amenities = {};
 
-  // Listen for changes on each input checkbox tag
-  $('input[type="checkbox"]').on('change', function () {
+  $('input[type="checkbox"]').on("change", function () {
     if (this.checked) {
-      // If the checkbox is checked, store the Amenity ID in the dictionary
-      amenities[$(this).data('id')] = $(this).data('name');
+      amenities[$(this).data("id")] = $(this).data("name");
     } else {
-      // If the checkbox is unchecked, remove the Amenity ID from the dictionary
-      delete amenities[$(this).data('id')];
+      delete amenities[$(this).data("id")];
     }
 
-    // Update the h4 tag inside the div Amenities with the list of Amenities checked
-    $('div.amenities h4').text(Object.values(amenities).join(', '));
+    $("div.amenities h4").text(Object.values(amenities).join(", "));
   });
 });
